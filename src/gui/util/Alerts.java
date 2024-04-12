661518730b2b7b2b14ed670cedb578e6d4fa6694
Jonas Avelino <jonasavma@gmail.com>
2024-04-12 00:14:42 -0300

    About view

---
+++ src/gui/util/Alerts.java
@@ -0,0 +1,18 @@
+package gui.util;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class Alerts {
+
+	public static void mostrarAlerta(String titulo, String cabecalho, String conteudo, AlertType tipo) {
+
+		Alert alert = new Alert(tipo);
+		alert.setTitle(titulo);
+		alert.setHeaderText(cabecalho);
+		alert.setContentText(conteudo);
+		alert.show();
+
+	}
+
+}
